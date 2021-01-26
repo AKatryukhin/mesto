2247df0bb0662abcd337ccf75bec832d452b1ce1
AKatryukhin <A.Katryukhin@yandex.ru>
2021-01-26 17:31:58 +0300

    feat: added button click

--- scripts/mesto.js
+++
@@ -1,4 +0,0 @@
-const popup = document.querySelector('.popup');
-const popupOpenButton = ducument.querySelector('.profile__edit');
-
-console.log(popupOpenButton);
---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const popup = document.querySelector('.popup');
+const popupOpenButton = document.querySelector('.profile__edit');
+const popupCloseButton = popup.querySelector('.popup__close');
+
+const togglePopup = function () {
+  popup.classList.toggle('popup_opened');
+}
+
+popupOpenButton.addEventListener('click', togglePopup);
+
+popupCloseButton.addEventListener('click', togglePopup);
+
+
