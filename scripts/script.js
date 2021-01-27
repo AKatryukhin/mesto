27b21042a3eb75a40486ecafd56dbf7bba6934a5
AKatryukhin <A.Katryukhin@yandex.ru>
2021-01-27 09:29:36 +0300

    feature/feat likeButton

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit');
 const popupCloseButton = popup.querySelector('.popup__close');
 
-const togglePopup = function () {
+let togglePopup = function () {
   popup.classList.toggle('popup_opened');
 }
 
@@ -14,10 +14,21 @@ const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('.popup__input-name');
 const jobInput = formElement.querySelector('popup__input-job');
 
-const formSubmitHandler = function(evt) {
-  evt.preventDefault();
+//const formSubmitHandler = function(evt) {
+ // evt.preventDefault();
   //let jobInputValue = jobInput.value;
 
 }
+
+const saveButton = popup.querySelector('.popup__save');
+
+const likeButton = document.querySelector('.photo__like');
+let toggleLikeButton = function (evt) {
+  likeButton.classList.toggle('.photo__like_type_active');
+  likeButton.classList.toggle('.photo__like_type_disabled');
+}
+
+likeButton.addEventListener('click',toggleLikeButton);
+
 
 //console.log(jobInputValue);
