393a0f55da039de6cb0bb445f58ef956c2f08965
AKatryukhin <A.Katryukhin@yandex.ru>
2021-01-27 11:30:37 +0300

    style: rename const to let by checklist

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__edit');
-const popupCloseButton = popup.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.profile__edit');
+let popupCloseButton = popup.querySelector('.popup__close');
 
 let togglePopup = function () {
   popup.classList.toggle('popup_opened');
@@ -10,14 +10,14 @@ popupOpenButton.addEventListener('click', togglePopup);
 
 popupCloseButton.addEventListener('click', togglePopup);
 
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input-name');
-const jobInput = formElement.querySelector('.popup__input-job');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input-name');
+let jobInput = formElement.querySelector('.popup__input-job');
 
 let formSubmitHandler = function(evt) {
   evt.preventDefault();
-  const ProfileName =  document.querySelector('.profile__name');
-  const ProfileJob =  document.querySelector('.profile__job');
+  let ProfileName =  document.querySelector('.profile__name');
+  let ProfileJob =  document.querySelector('.profile__job');
   ProfileName.textContent = nameInput.value;
   ProfileJob.textContent = jobInput.value;
   togglePopup();
