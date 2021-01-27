636882798cbb8b96852767e59796ed53f089c769
AKatryukhin <A.Katryukhin@yandex.ru>
2021-01-27 12:29:31 +0300

    style: beauty

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,31 @@
-let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__edit');
-let popupCloseButton = popup.querySelector('.popup__close');
+let popup = document.querySelector(".popup");
+let popupOpenButton = document.querySelector(".profile__edit");
+let popupCloseButton = popup.querySelector(".popup__close");
 
 let togglePopup = function () {
-  popup.classList.toggle('popup_opened');
-}
-
-popupOpenButton.addEventListener('click', togglePopup);
-
-popupCloseButton.addEventListener('click', togglePopup);
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-job');
-
-let formSubmitHandler = function(evt) {
-  evt.preventDefault();
-  let ProfileName =  document.querySelector('.profile__name');
-  let ProfileJob =  document.querySelector('.profile__job');
-  ProfileName.textContent = nameInput.value;
-  ProfileJob.textContent = jobInput.value;
-  togglePopup();
+    popup.classList.toggle("popup_opened");
 };
 
-formElement.addEventListener('submit', formSubmitHandler);
+popupOpenButton.addEventListener("click", togglePopup);
 
+popupCloseButton.addEventListener("click", togglePopup);
 
+let formElement = document.querySelector(".popup__form");
+let nameInput = formElement.querySelector(".popup__input-name");
+let jobInput = formElement.querySelector(".popup__input-job");
 
-/*const likeButton = document.querySelector('.photo__like');
+let formSubmitHandler = function (evt) {
+    evt.preventDefault();
+    let ProfileName = document.querySelector(".profile__name");
+    let ProfileJob = document.querySelector(".profile__job");
+    ProfileName.textContent = nameInput.value;
+    ProfileJob.textContent = jobInput.value;
+    togglePopup();
+};
+
+formElement.addEventListener("submit", formSubmitHandler);
+
+/*const likeButton = document.querySelectorAll('.photo__like');
 
 let toggleLikeButton = function (evt) {
   likeButton.classList.toggle('photo__like_type_disabled');
