e4acc5e8983436cc9d5f850902438c20630d2bb0
AKatryukhin <A.Katryukhin@yandex.ru>
2021-01-29 13:53:30 +0300

    style: js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
-let popup = document.querySelector(".popup"); //нашел попап
-let popupOpenButton = document.querySelector(".profile__edit"); //нашел кнопку открытия попапа - редактирования профиля
-let popupCloseButton = popup.querySelector(".popup__close"); // нашел кнопку закрытия попапа
-let formElement = document.querySelector(".popup__form"); // нашел форму попапа
-let nameInput = formElement.querySelector(".popup__input_type_name"); // нашел поле ввода имени в форме попап
-let jobInput = formElement.querySelector(".popup__input_type_job"); //нашел поле ввода "о себе" в форме попап
-let profileName = document.querySelector(".profile__name"); // нашел поле для отображения имени в профиле
-let profileJob = document.querySelector(".profile__job"); // нашел поле для отображения "о себе" в профиле
+let popup = document.querySelector('.popup'); //нашел попап
+let popupOpenButton = document.querySelector('.profile__edit'); //нашел кнопку открытия попапа - редактирования профиля
+let popupCloseButton = popup.querySelector('.popup__close'); // нашел кнопку закрытия попапа
+let formElement = document.querySelector('.popup__form'); // нашел форму попапа
+let nameInput = formElement.querySelector('.popup__input_type_name'); // нашел поле ввода имени в форме попап
+let jobInput = formElement.querySelector('.popup__input_type_job'); //нашел поле ввода "о себе" в форме попап
+let profileName = document.querySelector('.profile__name'); // нашел поле для отображения имени в профиле
+let profileJob = document.querySelector('.profile__job'); // нашел поле для отображения "о себе" в профиле
 
 let openPopup = function () {
-    popup.classList.add("popup_opened");
+    popup.classList.add('popup_opened');
 }; // создал переменную с функцией открытия попапа
 
 let closePopup = function () {
-    popup.classList.remove("popup_opened");
+    popup.classList.remove('popup_opened');
 }; //создал переменную с функцией закрытия попапа
 
 let formSubmitHandler = function (evt) {
@@ -22,7 +22,7 @@ let formSubmitHandler = function (evt) {
     closePopup();
 }; //создал переменную с функцией, которая заносит введенные в поля ввода формы данных в профиль
 
-popupOpenButton.addEventListener("click", openPopup); // добавил слушателя кнопке открытия попапа - редактирования профиля
-popupCloseButton.addEventListener("click", closePopup); // добавил слушателя кнопке закрытия попапа
-formElement.addEventListener("submit", formSubmitHandler); // добавил слушателя отправке формы
+popupOpenButton.addEventListener('click', openPopup); // добавил слушателя кнопке открытия попапа - редактирования профиля
+popupCloseButton.addEventListener('click', closePopup); // добавил слушателя кнопке закрытия попапа
+formElement.addEventListener('submit', formSubmitHandler); // добавил слушателя отправке формы
 
