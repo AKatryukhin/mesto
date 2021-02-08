053d1fe9488fe1032598672b838a02c5359c6067
AKatryukhin <A.Katryukhin@yandex.ru>
2021-02-08 10:08:08 +0300

    style: rename let to const

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
-let popup = document.querySelector('.popup'); //нашел попап
-let popupOpenButton = document.querySelector('.profile__edit'); //нашел кнопку открытия попапа - редактирования профиля
-let popupCloseButton = popup.querySelector('.popup__close'); // нашел кнопку закрытия попапа
-let formElement = document.querySelector('.popup__form'); // нашел форму попапа
-let nameInput = formElement.querySelector('.popup__input_type_name'); // нашел поле ввода имени в форме попап
-let jobInput = formElement.querySelector('.popup__input_type_job'); //нашел поле ввода "о себе" в форме попап
-let profileName = document.querySelector('.profile__name'); // нашел поле для отображения имени в профиле
-let profileJob = document.querySelector('.profile__job'); // нашел поле для отображения "о себе" в профиле
+const popup = document.querySelector('.popup'); //нашел попап
+const popupOpenButton = document.querySelector('.profile__edit'); //нашел кнопку открытия попапа - редактирования профиля
+const popupCloseButton = popup.querySelector('.popup__close'); // нашел кнопку закрытия попапа
+const formElement = document.querySelector('.popup__form'); // нашел форму попапа
+const nameInput = formElement.querySelector('.popup__input_type_name'); // нашел поле ввода имени в форме попап
+const jobInput = formElement.querySelector('.popup__input_type_job'); //нашел поле ввода "о себе" в форме попап
+const profileName = document.querySelector('.profile__name'); // нашел поле для отображения имени в профиле
+const profileJob = document.querySelector('.profile__job'); // нашел поле для отображения "о себе" в профиле
 
-let openPopup = function () {
+const openPopup = function () {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }; // создал переменную с функцией открытия попапа и присваивания инпутам значений  из профайла
 
-let closePopup = function () {
+const closePopup = function () {
     popup.classList.remove('popup_opened');
 }; //создал переменную с функцией закрытия попапа
 
-let formSubmitHandler = function (evt) {
+const formSubmitHandler = function (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
