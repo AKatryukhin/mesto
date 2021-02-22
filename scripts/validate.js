57288bdb6f2ddaa3e7c213872d83b8ea2bba1c82
AKatryukhin <A.Katryukhin@yandex.ru>
2021-02-22 17:51:50 +0300

    feat: added span-error to HTML

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save',
+  inactiveButtonClass: 'popup__save_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
