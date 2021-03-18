4d50a769f3f426fde653ec2513b5c882a706af9d
AKatryukhin <A.Katryukhin@yandex.ru>
2021-03-18 15:41:15 +0300

    feat: added _toggleButtonState to FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -8,7 +8,6 @@ class FormValidator {
     this._errorClass = data.errorClass;
 }
 
-
 _showInputError(inputElement) {
   const _errorElement = this._element.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(this._inputErrorClass);
@@ -23,7 +22,6 @@ _hideInputError(inputElement) {
   _errorElement.textContent = '';
 }
 
-
 _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
       this._showInputError(inputElement);
@@ -36,139 +34,35 @@ _setEventListeners() {
   this._inputList = Array.from(this._element.querySelectorAll(this._inputSelector));
 
   this._inputList.forEach((inputElement) => {
-     // const _buttonElement = this._element.querySelector(this._submitButtonSelector);
-//  проверяю состояние кнопки в начале
-  // this._toggleButtonState();
-  inputElement.addEventListener('input', () => {
+     const buttonElement = this._element.querySelector(this._submitButtonSelector);
+//  проверяю состояние кнопки вначале
+    this._toggleButtonState(inputElement, buttonElement);
+    inputElement.addEventListener('input', () => {
     this._checkInputValidity(inputElement);
-
-
 // проверяю состояние кнопки при изменении полей
-      // this._toggleButtonState();
+    this._toggleButtonState(inputElement, buttonElement);
+
     });
   });
 }
 
-
-
-
-
+// метод включения.выключения кнопок после проверки инпутов на валидность
+_toggleButtonState(inputElement, buttonElement) {
+  if (!inputElement.validity.valid) {
+      buttonElement.classList.add(this._inactiveButtonClass);
+      buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.classList.remove(this._inactiveButtonClass);
+    buttonElement.removeAttribute('disabled');
+  }
+}
 
 enableValidation() {
+  this._element.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+});
   this._setEventListeners();
-//   this._element.addEventListener('submit', (evt) => {
-//      evt.preventDefault();
-//      this._setEventListeners();
-// });
 }
-
-
-// проверяет наличие невалидных инпутов
-// _hasInvalidInput() {
-//   return this._inputList.some((_inputElement) => {
-//   return !_inputElement.validity.valid;
-// });
-// }
-
-
-// функция включения.выключения кнопок после проверки инпутов на валидность
-// _toggleButtonState() {
-//   if (_hasInvalidInput()) {
-//       _buttonElement.classList.add(this._inactiveButtonClass);
-//       _buttonElement.setAttribute('disabled', true);
-//   } else {
-//     _buttonElement.classList.remove(this._inactiveButtonClass);
-//     _buttonElement.removeAttribute('disabled');
-//   }
-// }
 }
-
-
-
-
-// //функция для отображения ошибки
-// const showInputError = (formElement, inputElement, errorMessage, inputErrorClass, errorClass) => {
-//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//   inputElement.classList.add(inputErrorClass);
-//   errorElement.textContent = errorMessage;
-//   errorElement.classList.add(errorClass);
-// };
-
-// //функция для скрытия ошибки
-// const hideInputError = (formElement, inputElement, inputErrorClass, errorClass) => {
-//   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//   inputElement.classList.remove(inputErrorClass);
-//   errorElement.classList.remove(errorClass);
-//   errorElement.textContent = '';
-// };
-
-// // функция для проверки на валидность инпутов
-// const checkInputValidity = (formElement, inputElement, inputErrorClass, errorClass) => {
-//   if (!inputElement.validity.valid) {
-//     showInputError(formElement, inputElement, inputElement.validationMessage, inputErrorClass, errorClass);
-//   } else {
-//     hideInputError(formElement, inputElement, inputErrorClass, errorClass);
-//   }
-// };
-
-// // функция для установки слушателей проверки на валидность инпутов
-// const setEventListeners = (formElement, inputSelector, submitButtonSelector,
-//   inactiveButtonClass, inputErrorClass, errorClass) => {
-//   const inputList = Array.from(formElement.querySelectorAll(inputSelector));
-//   const buttonElement = formElement.querySelector(submitButtonSelector);
-//  // проверяю состояние кнопки в начале
-//   toggleButtonState(inputList, buttonElement, inactiveButtonClass);
-
-//   inputList.forEach((inputElement) => {
-//     inputElement.addEventListener('input', function () {
-//       checkInputValidity(formElement, inputElement, inputErrorClass, errorClass);
-// // проверяю состояние кнопки при изменении полей
-//       toggleButtonState(inputList, buttonElement, inactiveButtonClass);
-//     });
-//   });
-// };
-
-// // функция, ответственная за включение валидации всех форм
-// const enableValidation = ({formSelector, inputSelector, submitButtonSelector,
-//   inactiveButtonClass, inputErrorClass, errorClass}) => {
-//  const formList = Array.from(document.querySelectorAll(formSelector));
-//  formList.forEach((formElement) => {
-//  formElement.addEventListener('submit', (evt) => {
-//      evt.preventDefault();
-//  });
-//  setEventListeners(formElement, inputSelector, submitButtonSelector,
-//    inactiveButtonClass, inputErrorClass, errorClass);
-//  });
-// };
-
-// // принимает на вход объект ключей и свойств для данного проекта
-// enableValidation(
-//  {
-//    formSelector: '.popup__form',
-//    inputSelector: '.popup__input',
-//    submitButtonSelector: '.popup__save',
-//    inactiveButtonClass: 'popup__save_inactive',
-//    inputErrorClass: 'popup__input_type_error',
-//    errorClass: 'popup__input-error_active'
-//  }
-// );
-
-// // проверяет наличие невалидных инпутов
-// function hasInvalidInput (inputList) {
-//   return inputList.some((inputElement) => {
-//   return !inputElement.validity.valid;
-// });
-// }
-
-// // функция включения.выключения кнопок после проверки инпутов на валидность
-// function toggleButtonState (inputList, buttonElement, inactiveButtonClass) {
-//   if (hasInvalidInput(inputList)) {
-//       buttonElement.classList.add(inactiveButtonClass);
-//       buttonElement.setAttribute('disabled', true);
-//   } else {
-//     buttonElement.classList.remove(inactiveButtonClass);
-//     buttonElement.removeAttribute('disabled');
-//   }
-// }
 
 export {FormValidator};
