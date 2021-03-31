2390320b0e4f491eda823ad02750efa0f632e764
AKatryukhin <A.Katryukhin@yandex.ru>
2021-03-31 14:20:39 +0300

    before index.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,50 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: '',
   },
-    mode: 'development',
+  mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
+    open: true,
     compress: true,
-    port: 8080,
-    open: true
+    port: 8080
   },
   module: {
     rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      exclude: '/node_modules/'
-    },
-    // добавили правило для обработки файлов
-    {
-      // регулярное выражение, которое ищет все файлы с такими расширениями
-      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-      type: 'asset/resource'
-    },
-    {
-      test: /\.css$/,
-    // при обработке этих файлов нужно использовать
-    // MiniCssExtractPlugin.loader и css-loader
-    use: [MiniCssExtractPlugin.loader, {
-      loader: 'css-loader'
-    }]
-  }
-  ]
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+
   ]
 };
